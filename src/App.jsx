fb2dd0310d1f7a15dbcbde01101f1a3613a2a4e1
RubyDragoon7197 <Andrespedrozab2004@gmail.com>
2025-09-14 20:49:59 +0000

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Booleano from './Pedroza/Booleano.jsx';
+import Estado from './Pedroza/Estado.jsx';
+import Lista from './Pedroza/lista.jsx';
+import Objeto from './Pedroza/Objeto.jsx';
+import String from './Pedroza/String.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Mis Componentes</h1>
+      <Booleano />
+      <Estado />
+      <Lista />
+      <Objeto />
+      <String />
+    </div>
+  );
 }
 
-export default App
+export default App;
